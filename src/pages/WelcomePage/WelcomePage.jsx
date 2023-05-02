2f4ad5422a6d42866cc21d94160eb43fe786d882
Henry Adams <henrybruce.adams@icloud.com>
2023-05-02 16:05:58 +0100

    Removed imports

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./WelcomePage.scss";
-import ClientCard from "../../components/ClientCard/ClientCard";
-import Staff from "../../components/Staff/Staff";
+
 const WelcomePage = () => {
   return (
     <div>
