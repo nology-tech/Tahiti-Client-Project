1404fc75bd66b7d878ae06991155992d7c0b1625
Alan-day <lesterthenovice@gmail.com>
2023-05-02 16:17:07 +0100

    Mobile version done. Two containers for picture and welcome form completed, initial positioning done.

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./WelcomePage.scss";
-
+import WelcomeImage from "../../assets/images/Background-img-welcome.svg";
 import Form from "../../Components/Form/Form";
 
 const WelcomePage = () => {
   return (
-    <div className="header-container">
-      <h1>Welcome Back </h1>
-      <p>
-        Welcome to New You clinic where we provide world class beauty treatments
-        that will make you look like you are 18 again!
-      </p>
-      <Form />
+    <div className="welcome-container">
+      <div className="header-container">
+        <h1>Welcome Back </h1>
+        <p>
+          Welcome to New You clinic where we provide world class beauty
+          treatments that will make you look like you are 18 again!
+        </p>
+        <Form />
+      </div>
+      <div className="picture-container">
+        <img src={WelcomeImage} />
+      </div>
     </div>
   );
 };
