593671fe5ecb8c8f578992efe821ed1efdc8349d
Alan-day <79362567+Alan-day@users.noreply.github.com>
2023-05-02 12:17:58 +0100

    Feature/13/project structure (#2)
    * boilerplate code and structure for Client Staff and Welcome Home pages
    
    * Created boiler plate for Client,Booking,Setting pages and routes added
    
    * created all routes for pages
    
    * De
    leted css files, imported all the components that needed for the pag
    es, added the route for home page and re-route the WelcomePage to de
    fault and remove the router tag.
    
    * deleted unwanted css files, add routes to home page and WelcomePage and imported components for every pages to App.jsx
    
    * Added insignificant changes to update 'De' comment
    
    * Deleted the unwanted css files in styles folder
    
    * Deleted ClientDelete, ClientSearch and ClientSorting pages
    
    ---------
    
    Co-authored-by: Piyaporn <piyapornlahr@gmail.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,13 +5,6 @@ const Home = () => {
     <div className="layout">
       <h1>Piya made this change</h1>
       <h2>Section Heading</h2>
-      <h3>Panel Heading</h3>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-        dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-        maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
-        necessitatibus.
-      </p>
     </div>
   );
 };
---
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./WelcomePage.scss";
+const WelcomePage = () => {
+  return (
+    <div>
+      <h1>Welcome Back </h1>
+      <h2>Welcome message</h2>
+    </div>
+  );
+};
+export default WelcomePage;
