a0d57d305a34b78bcd04ed998b2b2eff00993aa6
Henry Adams <henrybruce.adams@icloud.com>
2023-05-02 15:25:25 +0100

    Added new branch, added SideNav and components for it

---
+++ src/SideNav/Components/Components.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Components.scss";
+
+const Components = (name, image) => {
+  return (
+    <div className="SideNav">
+      <img src={image} alt="" />
+      <h3>{name}</h3>
+    </div>
+  );
+};
+
+export default Components;
