4267ef72b20e6a81dace0b6d8103b76ccecfc7c3
Liana Petrova <lianapetrova00@gmail.com>
2023-05-02 10:14:44 +0100

    Styled staff card component

--- src/components/Staff/Staff.jsx
+++ src/components/Staff/Staff.jsx
@@ -1,17 +1,17 @@
 import "./Staff.scss";
 import React from "react";
-import "../../assets/images/Staff Image.svg";
+import StaffImage from "../../assets/images/Staff Image.svg"
 
 const Staff = () => {
   return (
     <div className="staff-card">
       <img
         className="staff-card__img"
-        src="../../assets/images/Staff Image.svg"
+        src={StaffImage}
         alt=""
       />
       <h1 className="staff-card__name">Staff 01</h1>
-      <h3 className="staff-card-title">Dev</h3>
+      <h3 className="staff-card__title">Dev</h3>
     </div>
   );
 };
