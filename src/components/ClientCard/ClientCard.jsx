dedb0717f81c5d065e3c40529681e5540964adfc
Henry Adams <henrybruce.adams@icloud.com>
2023-05-02 15:54:28 +0100

    changed link tag to be a tag

--- src/components/ClientCard/ClientCard.jsx
+++ src/components/ClientCard/ClientCard.jsx
@@ -1,15 +1,15 @@
 import "./ClientCard.scss";
 import React from "react";
-import ClientImage from "../../assets/data/mockClientData"
+import ClientImage from "../../assets/data/mockClientData";
 
 const ClientCard = ({ name }) => {
   return (
     <div className="client-card">
       <img className="client-card__img" src={ClientImage} alt="client-image" />
       <h1 className="client-card__name">{name}</h1>
-      <Link className="client-card__title" to="">
+      <a href="" className="client-card__title" to="">
         Edit Client
-      </Link>
+      </a>
     </div>
   );
 };
