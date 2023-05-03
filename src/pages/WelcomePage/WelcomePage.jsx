3e1ef761ce4500d1dda062f29310b5bd77e0b7b6
Alan-day <lesterthenovice@gmail.com>
2023-05-03 09:47:37 +0100

    logo added, new container for logo created

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -2,25 +2,34 @@ import React from "react";
 import "./WelcomePage.scss";
 import WelcomeImage from "../../assets/images/Background-img-welcome.svg";
 import Form from "../../Components/Form/Form";
-
+import Logo from "../../assets/images/logo black.svg";
 
 const WelcomePage = () => {
   return (
-    <div className="welcome-container">
-      <div className="header-container">
-        <h1>Welcome Back </h1>
-        <p>
-          Welcome to New You clinic where we provide world class beauty
-          treatments that will make you look like you are 18 again!
-        </p>
-        <Form />
-        <button>Log in</button>
-      </div>
-      <img src={WelcomeImage} />
-      {/* <div className="picture-container">
+    <>
+      <div className="welcome-container">
+        <div className="logo-container">
+          <img src={Logo} alt="logo" className="picture-logo" />
+        </div>
+        <div className="header-container">
+          <h1>Welcome Back </h1>
+          <p>
+            Welcome to New You clinic where we provide world class beauty
+            treatments that will make you look like you are 18 again!
+          </p>
+          <Form />
+          <button>Log in</button>
+        </div>
+        <img
+          src={WelcomeImage}
+          className="picture-main"
+          alt="blurry image of a class"
+        />
+        {/* <div className="picture-container">
         <img src={WelcomeImage} />
       </div> */}
-    </div>
+      </div>
+    </>
   );
 };
 export default WelcomePage;
