98573be0c7e22f2cd9b43af51473abd1307e8823
Alan-day <lesterthenovice@gmail.com>
2023-05-03 14:04:34 +0100

    user and staff data created

--- src/assets/data/mockClientData.js
+++ src/assets/data/mockClientData.js
@@ -1,8 +1,117 @@
 import ClientImage from "../images/client-image.svg";
 
-const clientData = {
-  name: "Louis Thiel",
-  clientImage: ClientImage,
-};
+const clientData = [
+  {
+    name: "Felix Tompson",
+    ClientImage: { ClientImage },
+    id: 1,
+    dateOfBirth: "12/12/1997",
+    email: "felixtompson@gmail.com",
+    mobileNumber: "+44 745455097",
+    practitioner: "Jane Dow",
 
+    bookings: {
+      bookingdDate: "12/12/2023",
+      bookingTime: "12:30",
+    },
+  },
+  {
+    name: "Sarah Johnson",
+    ClientImage: { ClientImage },
+    id: 2,
+    dateOfBirth: "06/02/1982",
+    email: "sarahjohnson@gmail.com",
+    mobileNumber: "+44 712345678",
+    practitioner: "John Smith",
+
+    bookings: {
+      bookingdDate: "01/01/2023",
+      bookingTime: "10:00",
+    },
+  },
+  {
+    name: "David Lee",
+    ClientImage: { ClientImage },
+    id: 3,
+    dateOfBirth: "10/05/1990",
+    email: "davidlee@gmail.com",
+    mobileNumber: "+44 789012345",
+    practitioner: "Jane Dow",
+
+    bookings: {
+      bookingdDate: "06/06/2023",
+      bookingTime: "14:00",
+    },
+  },
+  {
+    name: "Emma Wilson",
+    ClientImage: { ClientImage },
+    id: 4,
+    dateOfBirth: "07/09/1987",
+    email: "emmawilson@gmail.com",
+    mobileNumber: "+44 745678901",
+    practitioner: "John Smith",
+
+    bookings: {
+      bookingdDate: "08/08/2023",
+      bookingTime: "16:00",
+    },
+  },
+  {
+    name: "Tom Jackson",
+    ClientImage: { ClientImage },
+    id: 5,
+    dateOfBirth: "03/11/1981",
+    email: "tomjackson@gmail.com",
+    mobileNumber: "+44 712345678",
+    practitioner: "Jane Dow",
+
+    bookings: {
+      bookingdDate: "02/02/2023",
+      bookingTime: "09:30",
+    },
+  },
+  {
+    name: "Oliver Green",
+    ClientImage: { ClientImage },
+    id: 6,
+    dateOfBirth: "12/08/1978",
+    email: "olivergreen@gmail.com",
+    mobileNumber: "+44 789012345",
+    practitioner: "John Smith",
+
+    bookings: {
+      bookingdDate: "07/07/2023",
+      bookingTime: "11:00",
+    },
+  },
+  {
+    name: "Sophie Brown",
+    ClientImage: { ClientImage },
+    id: 7,
+    dateOfBirth: "04/03/1993",
+    email: "sophiebrown@gmail.com",
+    mobileNumber: "+44 745678901",
+    practitioner: "Jane Dow",
+
+    bookings: {
+      bookingdDate: "05/05/2023",
+      bookingTime: "15:00",
+    },
+  },
+  {
+    name: "James Taylor",
+    ClientImage: { ClientImage },
+    id: 8,
+    dateOfBirth: "09/12/1980",
+    email: "jamestaylor@gmail.com",
+    mobileNumber: "+44 712345678",
+    practitioner: "John Smith",
+
+    bookings: {
+      bookingdDate: "04/04/2023",
+      bookingTime: "13:00",
+    },
+  },
+];
 export default clientData;
--- src/assets/data/mockStaffData.js
+++ src/assets/data/mockStaffData.js
@@ -1,9 +1,114 @@
 import staffImage from "../images/staff-img.svg";
 
-const staffData = {
-  staffNumber: "01",
-  staffImage: staffImage,
-  jobTitle: "Full Time Employee",
-};
+const staffData = [
+  {
+    fullName: "Tom Tompson",
+    staffNumber: "1",
+    staffImage: { staffImage },
+    role: "Admin",
+    employeeOverview: "dsdasdff",
+    startDate: "09/09/2021",
+    courseCompletion: "10/10/2020",
+    manager: "Jon Doe",
+    jobTitle: "Full Time Employee",
+
+    resources: {
+      resourceName: "notepad",
+      resourceId: 1,
+      quantity: 20,
+      autoPurchase: "YES",
+      constPerUnit: "£1.99",
+      autoPurchaseLevel: 10,
+      category: "admin",
+    },
+  },
+  {
+    fullName: "John Smith",
+    staffNumber: "2",
+    staffImage: { staffImage },
+    role: "Manager",
+    employeeOverview: "dsdasdff",
+    startDate: "10/10/2020",
+    courseCompletion: "11/11/2021",
+    manager: "Jane Doe",
+    jobTitle: "Full Time Employee",
+
+    resources: {
+      resourceName: "pen",
+      resourceId: 2,
+      quantity: 50,
+      autoPurchase: "NO",
+      constPerUnit: "£0.99",
+      autoPurchaseLevel: 0,
+      category: "admin",
+    },
+  },
+  {
+    fullName: "Jane Doe",
+    staffNumber: "3",
+    staffImage: { staffImage },
+    role: "HR Manager",
+    employeeOverview: "dsdasdff",
+    startDate: "12/12/2019",
+    courseCompletion: "01/01/2020",
+    manager: "Admin",
+    jobTitle: "Full Time Employee",
+
+    resources: {
+      resourceName: "mouse pad",
+      resourceId: 3,
+      quantity: 10,
+      autoPurchase: "YES",
+      constPerUnit: "£2.49",
+      autoPurchaseLevel: 5,
+      category: "admin",
+    },
+  },
+  {
+    fullName: "Amy Johnson",
+    staffNumber: "4",
+    staffImage: { staffImage },
+    role: "Customer Service Rep",
+    employeeOverview: "dsdasdff",
+    startDate: "03/03/2022",
+    courseCompletion: "N/A",
+    manager: "Tom Tompson",
+    jobTitle: "Part Time Employee",
+
+    resources: {
+      resourceName: "printer paper",
+      resourceId: 2,
+      quantity: 100,
+      autoPurchase: "NO",
+      constPerUnit: "£0.20",
+      autoPurchaseLevel: 0,
+      category: "customer service",
+    },
+  },
+  {
+    fullName: "Mike Williams",
+    staffNumber: { staffImage },
+    staffImage: "poi",
+    role: "Software Developer",
+    employeeOverview: "dsdasdff",
+    startDate: "04/04/2023",
+    courseCompletion: "02/02/2023",
+    manager: "John Smith",
+    jobTitle: "Full Time Employee",
+
+    resources: {
+      resourceName: "laptop",
+      resourceId: 1,
+      quantity: 1,
+      autoPurchase: "NO",
+      constPerUnit: "£1299.00",
+      autoPurchaseLevel: 0,
+      category: "software development",
+    },
+  },
+];
+
+// staffImage: staffImage,
+// jobTitle: "Full Time Employee",
 
 export default staffData;
