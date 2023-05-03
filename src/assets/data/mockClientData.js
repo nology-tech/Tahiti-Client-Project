52edf48c162d6d60e788640615c1ee690976ffae
Piyaporn Lahr <125895065+pilahr@users.noreply.github.com>
2023-05-03 15:29:29 +0100

    Feature/18/mock data (#7)
    * user and staff data created
    
    * Mock data arranged and cleaned
    
    * pictures for staff and clients provided, all mock data in the same file
    
    * import for image deleted
    
    ---------
    
    Co-authored-by: Alan-day <lesterthenovice@gmail.com>

--- src/assets/data/mockClientData.js
+++
@@ -1,8 +0,0 @@
-import ClientImage from "../images/client-image.svg";
-
-const clientData = {
-  name: "Louis Thiel",
-  clientImage: ClientImage,
-};
-
-export default clientData;
--- src/assets/data/mockStaffData.js
+++
@@ -1,9 +0,0 @@
-import staffImage from "../images/staff-img.svg";
-
-const staffData = {
-  staffNumber: "01",
-  staffImage: staffImage,
-  jobTitle: "Full Time Employee",
-};
-
-export default staffData;
