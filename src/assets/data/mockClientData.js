77ddbc298158a8f8491e2d5c3e3007fb7b2f77c9
Piyaporn <piyapornlahr@gmail.com>
2023-05-03 14:38:18 +0100

    Mock data arranged and cleaned

--- src/assets/data/mockClientData.js
+++ src/assets/data/mockClientData.js
@@ -11,7 +11,8 @@ const clientData = [
     practitioner: "Jane Dow",
 
     bookings: {
-      bookingdDate: "12/12/2023",
+      practitioner: "John Smith",
+      bookingDate: "12/12/2023",
       bookingTime: "12:30",
     },
   },
@@ -22,10 +23,10 @@ const clientData = [
     dateOfBirth: "06/02/1982",
     email: "sarahjohnson@gmail.com",
     mobileNumber: "+44 712345678",
-    practitioner: "John Smith",
 
     bookings: {
-      bookingdDate: "01/01/2023",
+      practitioner: "John Smith",
+      bookingDate: "01/01/2023",
       bookingTime: "10:00",
     },
   },
@@ -36,10 +37,10 @@ const clientData = [
     dateOfBirth: "10/05/1990",
     email: "davidlee@gmail.com",
     mobileNumber: "+44 789012345",
-    practitioner: "Jane Dow",
 
     bookings: {
-      bookingdDate: "06/06/2023",
+      practitioner: "Tom Tompson",
+      bookingDate: "06/06/2023",
       bookingTime: "14:00",
     },
   },
@@ -50,10 +51,10 @@ const clientData = [
     dateOfBirth: "07/09/1987",
     email: "emmawilson@gmail.com",
     mobileNumber: "+44 745678901",
-    practitioner: "John Smith",
 
     bookings: {
-      bookingdDate: "08/08/2023",
+      practitioner: "Jane Dory",
+      bookingDate: "08/08/2023",
       bookingTime: "16:00",
     },
   },
@@ -64,10 +65,10 @@ const clientData = [
     dateOfBirth: "03/11/1981",
     email: "tomjackson@gmail.com",
     mobileNumber: "+44 712345678",
-    practitioner: "Jane Dow",
 
     bookings: {
-      bookingdDate: "02/02/2023",
+      practitioner: "Amy Johnson",
+      bookingDate: "02/02/2023",
       bookingTime: "09:30",
     },
   },
@@ -78,10 +79,10 @@ const clientData = [
     dateOfBirth: "12/08/1978",
     email: "olivergreen@gmail.com",
     mobileNumber: "+44 789012345",
-    practitioner: "John Smith",
 
     bookings: {
-      bookingdDate: "07/07/2023",
+      practitioner: "Mike Williams",
+      bookingDate: "07/07/2023",
       bookingTime: "11:00",
     },
   },
@@ -92,11 +93,11 @@ const clientData = [
     dateOfBirth: "04/03/1993",
     email: "sophiebrown@gmail.com",
     mobileNumber: "+44 745678901",
-    practitioner: "Jane Dow",
 
     bookings: {
-      bookingdDate: "05/05/2023",
-      bookingTime: "15:00",
+      practitioner: "Mike Williams",
+      bookingDate: "07/07/2023",
+      bookingTime: "11:30",
     },
   },
   {
@@ -106,10 +107,10 @@ const clientData = [
     dateOfBirth: "09/12/1980",
     email: "jamestaylor@gmail.com",
     mobileNumber: "+44 712345678",
-    practitioner: "John Smith",
 
     bookings: {
-      bookingdDate: "04/04/2023",
+      practitioner: "Amy Johnson",
+      bookingDate: "04/04/2023",
       bookingTime: "13:00",
     },
   },
