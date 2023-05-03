d4ca4c05b28310faa19cda492c65682f0c253bcb
Alan-day <lesterthenovice@gmail.com>
2023-05-03 16:57:09 +0100

    initial styling for table done, testing done on clients page, colours and rounded corners applied

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,8 +3,8 @@ import "./Table.scss";
 
 const Table = ({ column1, column2, column3, column4, column5, title }) => {
   return (
-    <div>
-        <h2>{title}</h2>
+    <div className="table">
+      <h2>{title}</h2>
       <table>
         <tr>
           <th>{column1}</th>
@@ -13,26 +13,26 @@ const Table = ({ column1, column2, column3, column4, column5, title }) => {
           <th>{column4}</th>
           <th>{column5}</th>
         </tr>
-        <tr>
-          <td>Row 1, Column 1</td>
-          <td>Row 1, Column 2</td>
-          <td>Row 1, Column 3</td>
-          <td>Row 1, Column 4</td>
-          <td>Row 1, Column 5</td>
+        <tr className="first-row">
+          <td> name1 </td>
+          <td>DOB1 </td>
+          <td>email1</td>
+          <td>mobile number1</td>
+          <td>practitioner1</td>
         </tr>
-        <tr>
-          <td>Row 2, Column 1</td>
-          <td>Row 2, Column 2</td>
-          <td>Row 2, Column 3</td>
-          <td>Row 2, Column 4</td>
-          <td>Row 2, Column 5</td>
+        <tr className="second-row">
+          <td> name2 </td>
+          <td>DOB2 </td>
+          <td>email2</td>
+          <td>mobile number2</td>
+          <td>practitioner2</td>
         </tr>
-        <tr>
-          <td>Row 3, Column 1</td>
-          <td>Row 3, Column 2</td>
-          <td>Row 3, Column 3</td>
-          <td>Row 3, Column 4</td>
-          <td>Row 3, Column 5</td>
+        <tr className="third-row">
+          <td> name3 </td>
+          <td>DOB3 </td>
+          <td>email3</td>
+          <td>mobile number3</td>
+          <td>practitioner3</td>
         </tr>
       </table>
     </div>
