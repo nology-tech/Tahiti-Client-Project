f7cf35551cb0c5af0fb381a7c48701fd2d68e392
Piyaporn <piyapornlahr@gmail.com>
2023-05-04 10:44:52 +0100

    Created the head container of the table with the icons and search box

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Table.scss";
+import hamburgerIcon from "../../assets/images/hamburger-icon.svg";
+import gridIcon from "../../assets/images/grid-icon.svg";
+import sortIcon from "../../assets/images/sort-icon.svg";
+import filterIcon from "../../assets/images/filter-icon.svg";
+import searchIcon from "../../assets/images/search-icon.svg";
 
 const Table = ({
   column1,
@@ -12,7 +17,36 @@ const Table = ({
 }) => {
   return (
     <div>
-      <h2 id="table-title">{title}</h2>
+      <div className="table-head-container">
+        <div className="table-head-container__title">
+          <h2 id="table-title">{title}</h2>
+          <img src={hamburgerIcon} alt="hamburger icon" />
+          <img src={gridIcon} alt="grid icon" />
+        </div>
+        <div className="table-head-container__sorting">
+          <div className="table-head-container__sorting--searchbox">
+            <input
+              className="table-head-container__sorting--input"
+              type="text"
+            />
+            <img
+              className="table-head-container__sorting--search"
+              src={searchIcon}
+              alt="search icon"
+            />
+          </div>
+
+          <div className="table-head-container__sorting--sort">
+            <img src={sortIcon} alt="sort icon" />
+            <p>Sort</p>
+          </div>
+          <div className="table-head-container__sorting--filter">
+            <img src={filterIcon} alt="filter icon" />
+            <p>Filter</p>
+          </div>
+        </div>
+      </div>
+
       <h3 id="table-header">{header}</h3>
       <div className="table-container">
         <table>
@@ -31,18 +65,18 @@ const Table = ({
             <td>practitioner1</td>
           </tr>
           <tr className="second-row">
-            <td className="first-column"> name2 </td>
-            <td>DOB2 </td>
-            <td>email2</td>
-            <td>mobile number2</td>
-            <td>practitioner2</td>
+            <td className="first-row__first-column"> name1 </td>
+            <td>DOB1 </td>
+            <td>email1</td>
+            <td className="first-row__bold-column">mobile number1</td>
+            <td>practitioner1</td>
           </tr>
           <tr className="third-row">
-            <td className="first-column"> name3 </td>
-            <td>DOB3 </td>
-            <td>email3</td>
-            <td>mobile number3</td>
-            <td>practitioner3</td>
+            <td className="first-row__first-column"> name1 </td>
+            <td>DOB1 </td>
+            <td>email1</td>
+            <td className="first-row__bold-column">mobile number1</td>
+            <td>practitioner1</td>
           </tr>
         </table>
       </div>
