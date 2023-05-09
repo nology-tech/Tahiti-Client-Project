a51959b8cb96226bbb8cfcf9f4a3f5c3c14f8230
Henry Adams <henrybruce.adams@icloud.com>
2023-05-09 11:55:30 +0100

    Added further validation on inputs. Styled Error message

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./LoginForm.scss";
+import { useState } from "react";
 
 const LoginForm = ({ email, password, emailInput, passwordInput }) => {
+  const [emailErrorMessage, setEmailErrorMessage] = useState(false);
+
   const handleSetPassword = (e) => {
     passwordInput(e.target.value);
   };
+
   const handleSetEmail = (e) => {
     emailInput(e.target.value);
+    if (!e.target.value.includes("@" && ".")) {
+      setEmailErrorMessage(true);
+    } else {
+      setEmailErrorMessage(false);
+    }
   };
 
   return (
@@ -18,11 +27,15 @@ const LoginForm = ({ email, password, emailInput, passwordInput }) => {
         type="text"
         value={email}
       />
+      <div className="form-container__error">
+        {emailErrorMessage && <p>Your email must be valid...</p>}
+      </div>
+
       <label className="form-container__label">Password</label>
       <input
         className="form-container__input"
         onInput={handleSetPassword}
-        type="text"
+        type="password"
         value={password}
       />
     </div>
