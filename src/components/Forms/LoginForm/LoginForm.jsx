4dc217e871d8a6998ba5c47e30fb0af32fa1d9b6
Lia711 <125596830+Lia711@users.noreply.github.com>
2023-05-09 12:21:13 +0100

    Feature/21/create authentication (#12)
    * "Moved changes from SideNav branch"
    
    * Created function that deals with user login and goes to home page on submit if the input fields are not null
    
    * Added error message for when user doesn't type in an email and/or password
    
    * Started working on getting the firebase authentication to work
    
    * Created functions to authenticate a single user
    
    * Can now log in by accessing email from firebase
    
    * Login check using data from firebase user
    
    * Added logic for displaying error message
    
    * Added error message when user doesn't type in correct email and password.
    
    * Improved error message styling.
    
    * Merged authentication branch with updated main. All working.
    
    * Users can only access others pages if they pass authentication check.
    
    * Added further validation on inputs. Styled Error message
    
    * Changed email error to display red.
    
    ---------
    
    Co-authored-by: Henry Adams <henrybruce.adams@icloud.com>

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import "./LoginForm.scss";
+import { useState } from "react";
 
-const LoginForm = () => {
+const LoginForm = ({ email, password, emailInput, passwordInput }) => {
+  const [emailErrorMessage, setEmailErrorMessage] = useState(false);
+
+  const handleSetPassword = (e) => {
+    passwordInput(e.target.value);
+  };
+
+  const handleSetEmail = (e) => {
+    emailInput(e.target.value);
+    if (!e.target.value.includes("@" && ".")) {
+      setEmailErrorMessage(true);
+    } else {
+      setEmailErrorMessage(false);
+    }
+  };
+
   return (
     <div className="form-container">
       <label className="form-container__label">Email Address</label>
-      <input className="form-container__input" type="text" />
+      <input
+        className="form-container__input"
+        onInput={handleSetEmail}
+        type="text"
+        value={email}
+      />
+      <div>
+        {emailErrorMessage && (
+          <p className="form-container__error">Your email must be valid...</p>
+        )}
+      </div>
+
       <label className="form-container__label">Password</label>
-      <input className="form-container__input" type="text" />
+      <input
+        className="form-container__input"
+        onInput={handleSetPassword}
+        type="password"
+        value={password}
+      />
     </div>
   );
 };
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,8 +3,33 @@ import "./WelcomePage.scss";
 import WelcomeImage from "../../assets/images/Background-img-welcome.svg";
 import Logo from "../../assets/images/logo black.svg";
 import LoginForm from "../../components/Forms/LoginForm/LoginForm";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const WelcomePage = () => {
+const WelcomePage = ({ setUser }) => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
+  const navigate = useNavigate();
+
+  const getUser = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        navigate("/home");
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(!errorMessage);
+      });
+  };
+
+  const hideError = () => {
+    setErrorMessage(!errorMessage);
+  };
+
   return (
     <div className="container">
       <div className="logo-container">
@@ -17,16 +42,30 @@ const WelcomePage = () => {
             Welcome to New You clinic where we provide world class beauty
             treatments that will make you look like you are 18 again!
           </p>
-          <LoginForm />
-          <button>Log in</button>
+          <LoginForm
+            password={password}
+            email={email}
+            emailInput={setEmail}
+            passwordInput={setPassword}
+          />
+          <button onClick={getUser}>Log in</button>
         </div>
       </div>
-
       <img
         src={WelcomeImage}
         className="picture-main"
         alt="blurry image of a class"
       />
+      <div>
+        {errorMessage && (
+          <div className="displayMessage">
+            <h1 className="heading">
+              ❌Please enter a valid email and password!
+            </h1>
+            <button onClick={hideError}>Try again</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
