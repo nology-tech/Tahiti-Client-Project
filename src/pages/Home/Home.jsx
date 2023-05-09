db06093b6544114e32776de0e4e723152282f532
Alan-day <lesterthenovice@gmail.com>
2023-05-09 10:58:33 +0100

    created containers for rows in Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,11 +14,18 @@ const Home = () => {
         <div className="mockHeader">
           <h1>Home</h1>
         </div>
-        <HompageCard title="New Appointment" link="/booking" />
-        <HompageCard title="Staff Details" link="/staff" />
-        <HompageCard title="Client Details" link="clients" />
-        <HompageCard title="Resources" link="/resources" />
-        <HompageCard title="Settings" link="/settings" />
+
+        <div className="rowContainer">
+          <div className="firstRow">
+            <HompageCard title="New Appointment" link="/booking" />
+            <HompageCard title="Staff Details" link="/staff" />
+            <HompageCard title="Client Details" link="clients" />
+          </div>
+          <div className="secondRow">
+            <HompageCard title="Resources" link="/resources" />
+            <HompageCard title="Settings" link="/settings" />
+          </div>
+        </div>
       </div>
       <MobileNavPage />
     </div>
