3a62ff87080630dec7d9602737803bb4970b67d5
Henry Adams <henrybruce.adams@icloud.com>
2023-05-10 16:05:14 +0100

    Styled widget component to be repsonsive on mobile + desktop

--- src/components/SettingsWidget/SettingsWidget.jsx
+++ src/components/SettingsWidget/SettingsWidget.jsx
@@ -1,36 +1,57 @@
-import React from 'react'
-import "./SettingsWidget.scss"
-import Sun from "../../assets/images/sun-toggle-dark.svg"
-import Moon from "../../assets/images/moon-toggle-dark.svg"
+import React from "react";
+import "./SettingsWidget.scss";
+import Sun from "../../assets/images/sun-toggle-dark.svg";
+import Moon from "../../assets/images/moon-toggle-dark.svg";
+import { useState } from "react";
 
 const SettingsWidget = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [textTheme, setTextTheme] = useState(false);
+
+  const changeColorTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+  const changeTextTheme = () => {
+    setTextTheme(true);
+  };
+
+  const applyTheme = () => {
+    console.log(textTheme);
+  };
+
   return (
-    <div className='widget'>
-        <div className='colorMode'>
-            <h1>Light/Dark Mode:</h1>
-            <img src={Sun} alt="" />
-            <label className="switch">
-              <input type="checkbox"/>
-              <span className="slider round"></span>
-            </label>
-            <img src={Moon} alt="" />
-        </div>
-        <div className='textMode'>
-            <h1>Select Theme:</h1>
-            <div className='theme'>
-              <p>Modern</p>
-              <input name="select" className="selector" type="radio"/>
-            </div>
-            <div className='theme'>
-              <p>Tech</p>
-              <input name="select" className="selector" type="radio"/>
-            </div>
-        </div>
-        <button>Apply</button>
-        
+    <div className="widget">
+      <div className="colorMode">
+        <h1>Light/Dark Mode:</h1>
+        <img src={Sun} alt="" />
+        <label className="switch">
+          <input type="checkbox" onClick={changeColorTheme} />
+          <span className="slider round"></span>
+        </label>
+        <img src={Moon} alt="" />
+      </div>
 
+      <section className="textMode">
+        <h1>Select Theme:</h1>
+        <div className="theme">
+          <div className="theme-modern">
+            <p>Modern</p>
+            <input name="select" type="radio" onClick={changeTextTheme} />
+          </div>
+          <div className="theme-tech">
+            <p>Tech</p>
+            <input
+              name="select"
+              className="selector"
+              type="radio"
+              onClick={changeTextTheme}
+            />
+          </div>
+        </div>
+      </section>
+      <button onClick={applyTheme}>Apply</button>
     </div>
-  )
-}
+  );
+};
 
-export default SettingsWidget
+export default SettingsWidget;
