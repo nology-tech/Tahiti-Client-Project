d0fb277a6a094d86966dfa964ff7e12341efa365
Dipeshpatel2e <dipeshpatel2@yahoo.co.uk>
2023-05-10 11:36:00 +0100

    CSS and dropdown list  added to Topnav

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import "./TopNav.scss"
+import React, { useState } from "react";
+import "./TopNav.scss";
 
-const TopNav = ({heading, buttonTitle, showButton}) => {
+const TopNav = ({ heading, buttonTitle, showButton }) => {
+  const [staffChosen, setStaffChosen] = useState("");
+
+  const staffNames = ["Leonardo", "Raphael", "Michaelangelo", "Donatelo"];
+
+  const handleStaff = (event) => {
+    setStaffChosen(event.target.value);
+  };
 
   return (
-    <div className='topnav'>
-        <h1 className='topnav__heading'>{heading}</h1>
-        {showButton && <button className='topnav__button'>{buttonTitle}</button>}
+    <div className="topnav">
+      <div className="topnav__header">
+        <h1 className="topnav__heading">{heading}</h1>
+
+        {showButton && (
+          <button className="topnav__button">{buttonTitle}</button>
+        )}
+      </div>
+      <select
+        value={staffChosen}
+        onChange={handleStaff}
+        className="topnav__dropdown"
+      >
+        <option value="">Choose Staff</option>
+        {staffNames.map((staff, index) => (
+          <option key={index} value={staff}>
+            {staff}
+          </option>
+        ))}
+      </select>
     </div>
-  )
-}
+  );
+};
 
-export default TopNav
-
-
-
+export default TopNav;
