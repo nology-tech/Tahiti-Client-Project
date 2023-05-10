0d6c1daa1a8077cb10419c6eab6c3f8814081dbf
Henry Adams <henrybruce.adams@icloud.com>
2023-05-10 11:26:19 +0100

    Final changes to button components

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "./Booking.scss";
 import SideNav from "../../components/SideNav/SideNav";
-import Button from "../../components/Button/Button";
 
 const Booking = () => {
   return (
     <div>
       <h1>Booking</h1>
       <SideNav />
-      <div className="buttons">
-        <Button variant="grayMid" name="Cancel" />
-        <Button variant="yellowMid" name="Save" />
-      </div>
     </div>
   );
 };
