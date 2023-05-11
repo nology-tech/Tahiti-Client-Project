5f71c8b71adcb333b85613807fc5dff3d20a7739
Alan-day <79362567+Alan-day@users.noreply.github.com>
2023-05-11 16:50:36 +0100

    Feature/25/appointment booking form (#17)
    * Table component created and imported in Clients.jsx
    
    * initial styling for table done, testing done on clients page, colours and rounded corners applied
    
    * more styling to the table done, colours and fonts match the template.
    
    * Created the head container of the table with the icons and search box
    
    * new table created, displayed as two grids, styling done on all items
    
    * Created TableCard component to render the data, edited the styling for the Table and mapped the clients data to show on the table in Clients.jsx
    
    * table reused in resource and clients components, displaying all data for resources without dividing it by staff number
    
    * Edited the data mapping for the Resources page to show on the table, applied a few changes with styling to the table
    
    * changed bookings, looping over staff bookings to get the booking they are assigned to
    
    * Modified the mockData by created a bookingsData and modified a code in Staff.jsx
    
    * Edited the code in Staff.jsx
    
    * Edited the code in Clients page to map over and get the data
    
    * Aligned tables and modified position changes to NavSide
    
    * fixed navMenuMobile component to display buttons on the whole width of the page
    
    * created containers for rows in Home component
    
    * fixed hompepage card layout
    
    * applied mapping client's names to the client page on mobile version
    
    * Applied changes to the layout in Clients page
    
    * fixed breakpoints for clients page, centered home button.
    
    * mobile page for clients  and resources done, working on desktop-resources
    
    * Applied mobile version to Staff, Client and Resources pages and modified the styling for those pages
    
    * Applied some change in Resources.scss from mobile-table-header justify-content start to flex-start
    
    * Created AppointmentBookingForm component with elements and styling
    
    * imported calendar package, current/selected date displays in the console
    
    * calendar styling done, buttons applied
    
    * style changed for time's dropdown list in width and useState applied for each form input
    
    * Created a variable appointmentData to store input data from the form
    
    * styling changes applied, cancel button functionality
    
    * Created mobile version of the booking appointment form and imported another date picker package to use
    
    * 'attempted array of time available for appointment, curently showing one value'
    
    * styling of mobile booking form updated, new way of displaying times applied
    
    * height of the booking form adjusted
    
    * Applied some changes to the files, removed true from user useState and gave overflow-x to hidden in resources page
    
    * Removed white space from the desktop version and adjusted screen size for small laptop
    
    * Fixed the error with deploying
    
    * modified the styling and removed unnecsessary margin
    
    ---------
    
    Co-authored-by: Piyaporn <piyapornlahr@gmail.com>

---
+++ src/components/Forms/AppointmentBookingForm/AppointmentBookingForm.jsx
@@ -0,0 +1,194 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./AppointmentBookingForm.scss";
+import mockData from "../../../assets/data/mockData.js";
+import Button from "../../Button/Button";
+import "react-datepicker/dist/react-datepicker.css";
+
+const AppointmentBookingForm = () => {
+  const [bookingDate, onChange] = useState(new Date());
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [staffMember, setStaffMember] = useState("Tom Tompson");
+  const [time, setTime] = useState("9:00");
+
+  const [date, setStartDate] = useState(new Date());
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const appointmentData = {
+      firstName,
+      lastName,
+      email,
+      mobileNumber,
+      staffMember,
+      bookingDate,
+      date,
+      time,
+    };
+    console.log(appointmentData);
+    handleCancel();
+  };
+
+  const handleMobileSubmit = (event) => {
+    event.preventDefault();
+    const appointmentDataMobile = {
+      firstName,
+      lastName,
+      email,
+      mobileNumber,
+      staffMember,
+      date,
+      time,
+    };
+    console.log(appointmentDataMobile);
+    handleCancel();
+  };
+
+  const handleCancel = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setMobileNumber("");
+    setStaffMember("");
+    setTime("9:00");
+  };
+
+  const handleFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastName = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMobileNumber = (event) => {
+    setMobileNumber(event.target.value);
+  };
+
+  const handleStaffMember = (event) => {
+    setStaffMember(event.target.value);
+  };
+
+  const handleTime = (event) => {
+    setTime(event.target.value);
+  };
+
+  const timeSlots = [
+    "9:00",
+    "9:30",
+    "10:00",
+    "10:30",
+    "11:00",
+    "11:30",
+    "12:00",
+    "12:30",
+    "13:00",
+    "13:30",
+    "14:00",
+    "14:30",
+    "15:00",
+    "15:30",
+    "16:00",
+    "16:30",
+    "17:00",
+  ];
+
+  return (
+    <div className="booking-form">
+      <div className="booking-form__left">
+        <label className="booking-form__left--label">First Name</label>
+        <input
+          className="booking-form__left--input"
+          type="text"
+          value={firstName}
+          onChange={handleFirstName}
+        />
+        <label className="booking-form__left--label">Last Name</label>
+        <input
+          className="booking-form__left--input"
+          type="text"
+          value={lastName}
+          onChange={handleLastName}
+        />
+        <label className="booking-form__left--label">Email Address</label>
+        <input
+          className="booking-form__left--input"
+          type="text"
+          value={email}
+          onChange={handleEmail}
+        />
+        <label className="booking-form__left--label">Mobile Number</label>
+        <input
+          className="booking-form__left--input"
+          type="text"
+          value={mobileNumber}
+          onChange={handleMobileNumber}
+        />
+        <label className="booking-form__left--label">Staff Member</label>
+        <select
+          className="booking-form__left--select"
+          value={staffMember}
+          onChange={handleStaffMember}
+        >
+          {mockData.staffData.map((staff, index) => {
+            return <option key={index}>{staff.fullName}</option>;
+          })}
+        </select>
+        <div className="booking-form__left--buttons">
+          <Button name={"Cancel"} variant="grayMid" onClick={handleCancel} />
+          <Button name={"Save"} variant="yellowMid" onClick={handleSubmit} />
+        </div>
+      </div>
+      <div className="booking-form__right">
+        <Calendar
+          className="booking-form__right--desktop-calendar"
+          onChange={onChange}
+          value={bookingDate}
+        />
+        <label className="booking-form__right--dateLabel">Date:</label>
+        <DatePicker
+          className="mobile-date"
+          selected={date}
+          onChange={(date) => setStartDate(date)}
+        />
+        <label className="booking-form__right--label">Time:</label>
+
+        {/* <div> */}
+        <select
+          className="booking-form__right--select"
+          value={time}
+          onChange={handleTime}
+        >
+          {timeSlots.map((element) => {
+            return (
+              <>
+                <option>{element}</option>;
+              </>
+            );
+          })}
+          ;
+        </select>
+        {/* </div> */}
+      </div>
+      <div className="mobile-button">
+        <Button
+          name={"Submit"}
+          variant="yellowWide"
+          onClick={handleMobileSubmit}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentBookingForm;
--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Booking.scss";
 import SideNav from "../../components/SideNav/SideNav";
+import AppointmentBookingForm from "../../components/Forms/AppointmentBookingForm/AppointmentBookingForm";
 
 const Booking = () => {
   return (
-    <div>
-      <h1>Booking</h1>
-      <SideNav />
+    <div className="booking">
+      <div className="booking__nav">
+        <SideNav />
+      </div>
+
+      <div className="booking__main">
+        <div className="booking__heading">
+          <h1>Booking</h1>
+        </div>
+        <div className="booking__main--right">
+          <AppointmentBookingForm />
+        </div>
+      </div>
     </div>
   );
 };
