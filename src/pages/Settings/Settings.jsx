44fa3a66f7bbddeceb8c8cc3c80561a40fbd7bb0
henryadamsgit <119608914+henryadamsgit@users.noreply.github.com>
2023-05-11 11:10:06 +0100

    Feature/26/settings widget (#16)
    * Created settings widget component. Unstyled as of now.
    
    * Styled more of the widget and positioned it.
    
    * Added all components to settings widget. Styled to some degree.
    
    * Finalized most of styling for the widget with the correct colors.
    
    * Styled widget component to be repsonsive on mobile + desktop
    
    * Finished settings widget styling. State updates correctly when slides and buttons are clicked.
    
    * Added button component and styled it.
    
    * State updates (unfinished)
    
    * Completed settings widget
    
    * Removed whitespace
    
    ---------
    
    Co-authored-by: Liana Petrova <lianapetrova00@gmail.com>

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Settings.scss";
 import SideNav from "../../components/SideNav/SideNav";
+import SettingsWidget from "../../components/SettingsWidget/SettingsWidget";
 
 const Settings = () => {
   return (
-    <div>
-      <h1>Settings</h1>
-      <SideNav />
-    </div>
-  );
+  <div>
+    <h1>Settings</h1>
+    <SideNav/> 
+    <SettingsWidget/>
+  </div>
+  )
 };
 
 export default Settings;
