951b4e698d422fdf89a3b6f5187878288064ca81
Henry Adams <henrybruce.adams@icloud.com>
2023-05-11 15:36:25 +0100

    changes

--- src/components/ClientCard/ClientCard.jsx
+++ src/components/ClientCard/ClientCard.jsx
@@ -1,14 +1,25 @@
 import "./ClientCard.scss";
 import React from "react";
-import "../../assets/data/mockClientData";
+import { useParams } from "react-router-dom";
+import mockData from "../../assets/data/mockData";
 
-const ClientCard = ({ name, ClientImage }) => {
+const ClientCard = ({ mockData }) => {
+  const { id } = useParams();
+
+  const currentClient = mockData.find((client) => (client.id = id));
+
+  const { name, clientImage, email, dateOfBirth, mobileNumber } = currentClient;
+
   return (
     <div className="client-card">
-      <img className="client-card__img" src={ClientImage} alt="client-image" />
+      <img
+        className="client-card__img"
+        src={clientImage}
+        alt="client-image"
+      />
       <h1 className="client-card__name">{name}</h1>
       <a href="" className="client-card__title" to="">
-        Edit Client
+        Edit Enrollment
       </a>
     </div>
   );
--- src/pages/ClientActive/ClientActive.jsx
+++ src/pages/ClientActive/ClientActive.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import "./ClientActive.scss"
+import React from "react";
+import "./ClientActive.scss";
 import SideNav from "../../components/SideNav/SideNav";
-
+import ClientCard from "../../components/ClientCard/ClientCard";
+import { Link } from "react-router-dom";
+import mockData from "../../assets/data/mockData";
 
 const ClientActive = () => {
   return (
     <div>
       <h1>ClientActive</h1>
-      <SideNav/> 
+      <SideNav />
+      <Link to={`/clients-active/${mockData.client.id}`}>
+        <ClientCard />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default ClientActive
+export default ClientActive;
