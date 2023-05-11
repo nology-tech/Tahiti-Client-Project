83583ff6aee54b81bd8664d99ce017e2b649f89a
Piyaporn <piyapornlahr@gmail.com>
2023-05-11 11:56:51 +0100

    Created ResourceForm component for mobile device

--- src/components/Forms/ResourceForm/ResourceForm.jsx
+++ src/components/Forms/ResourceForm/ResourceForm.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Button from "../../Button/Button";
 import "./ResourceForm.scss";
 
 const ResourceForm = () => {
-  return <div>
-    <label>Quantity Remaining:</label>
-  </div>;
+  return (
+    <div className="resource-form-container">
+      <div className="resource-form">
+        <label className="resource-form__label">Quantity Remaining:</label>
+        <input className="resource-form__input" />
+        <label className="resource-form__label">Cost per Unit</label>
+        <input className="resource-form__input" />
+        <label className="resource-form__label">Auto-Purchase</label>
+        <div className="resource-form__radio-buttons">
+          <label className="resource-form__radio-buttons--label">Yes</label>
+          <input
+            className="resource-form__radio-buttons--button"
+            type="radio"
+          />
+          <label className="resource-form__radio-buttons--label">No</label>
+          <input
+            className="resource-form__radio-buttons--button"
+            type="radio"
+          />
+        </div>
+        <label className="resource-form__label">Auto-Purchase Level</label>
+        <input className="resource-form__input" />
+      </div>
+
+      <Button name="Save" image={false} variant="yellowWide" />
+    </div>
+  );
 };
 
 export default ResourceForm;
