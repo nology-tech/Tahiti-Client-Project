eedf91eaf903d1d41891f7e8d09ed3d132c25ea4
Henry Adams <henrybruce.adams@icloud.com>
2023-05-12 11:18:27 +0100

    Implemented topnav to client active page. Adjusted styles for SideNav

--- src/pages/ClientActive/ClientActive.jsx
+++ src/pages/ClientActive/ClientActive.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import "./ClientActive.scss";
 import SideNav from "../../components/SideNav/SideNav";
 import ClientCard from "../../components/ClientCard/ClientCard";
-import TopNav from '../../components/TopNav/TopNav';
+import TopNav from "../../components/TopNav/TopNav";
 
 const ClientActive = () => {
   return (
     <div className="clientActive">
       <SideNav />
-      <TopNav heading ="Clients" buttonTitle ="+ Edit" showButton={true}/>
-      <div className="content">
-        <h1 className="heading">Overview</h1>
-        <ClientCard className="cards"/>
+      <div className="main">
+        <TopNav heading="Clients" buttonTitle="+ Edit" showButton={true} />
+        <div className="content">
+          <h1 className="heading">Overview</h1>
+          <ClientCard className="cards" />
+        </div>
       </div>
-      </div>
-)}
+    </div>
+
+  );
+};
 
 export default ClientActive;
